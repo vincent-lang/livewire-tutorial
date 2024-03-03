739e0ff8793d06384bf4803c406789440dd41d75
vincent van de merwe <335479@student.mboutrecht.nl>
2024-03-03 22:23:37 +0100

    i am done

--- app/Livewire/SearchUsers.php
+++ app/Livewire/SearchUsers.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class SearchUsers extends Component
 {
     public $search = '';
 
+    protected $queryString = ['search'];
+
     public function render()
     {
-        return view('livewire.search-users');
+        return view('livewire.search-users', [
+            'users' => User::where('name', 'like', '%' . $this->search . '%')->get(),
+        ]);
     }
 }
--- resources/views/livewire/search-users.blade.php
+++ resources/views/livewire/search-users.blade.php
@@ -1,3 +1,13 @@
 <div>
-    <input wire:model.live="search">
+    <link rel="stylesheet" href="/css/app.css">
+
+    <input class="search" wire:model.live="search" type="search" placeholder="search a person....">
+
+    @foreach($users as $user)
+    <div class="user">
+        <li>
+            {{ $user->name }}
+        </li>
+    </div>
+    @endforeach
 </div>
